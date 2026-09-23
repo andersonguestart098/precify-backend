6256295754e02e3cbd29d04785429a976f8debe2
Anderson Henrique Tavares Anastácio <111889956+andersonguestart098@users.noreply.github.com>
2026-09-22 21:18:41 -0300

    query workspace favorites by type

--- src/main/java/br/com/buscaproduto/favorite/FavoriteRepository.java
+++ src/main/java/br/com/buscaproduto/favorite/FavoriteRepository.java
@@ -1,6 +1,9 @@
 package br.com.buscaproduto.favorite;
+
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 public interface FavoriteRepository extends MongoRepository<Favorite, String> {
     List<Favorite> findByUserId(String userId);
+    List<Favorite> findByUserIdAndEntityType(String userId, String entityType);
 }
